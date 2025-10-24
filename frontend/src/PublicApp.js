51c8a79c9bb688b4bc1a7fd7f841149958daed0a
Emergent AI <chavapalmarubin@hotmail.com>
2025-10-24 19:36:43 +0000

    auto-commit for 035d8095-221d-4ec9-aace-c745b0f0eb79

--- frontend/src/PublicApp.js
+++ frontend/src/PublicApp.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ProspectsPortal from './components/ProspectsPortal';
+import ProspectsPortalRouter from './components/ProspectsPortalRouter';
 
 function PublicApp() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/prospects/*" element={<ProspectsPortal />} />
-        <Route path="/prospects" element={<ProspectsPortal />} />
-        <Route path="*" element={<ProspectsPortal />} />
+        <Route path="/prospects/*" element={<ProspectsPortalRouter />} />
+        <Route path="*" element={<ProspectsPortalRouter />} />
       </Routes>
     </BrowserRouter>
   );
