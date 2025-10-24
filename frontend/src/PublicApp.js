d40e1beb10d2c8a69293d010f4f6f5faea6192b1
Emergent AI <chavapalmarubin@hotmail.com>
2025-10-24 18:53:14 +0000

    auto-commit for c8ccab54-4839-404d-bdad-609cf32d8c7a

--- frontend/src/PublicApp.js
+++ frontend/src/PublicApp.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ProspectsPortalNew from './components/ProspectsPortalNew';
+import ProspectsPortal from './components/ProspectsPortal';
 
 function PublicApp() {
   return (
