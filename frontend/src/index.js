9db114cdf5cc04b4f13937c0f1aa149d3b4f31fb
Emergent AI <chavapalmarubin@hotmail.com>
2025-10-31 17:05:21 +0000

    auto-commit for debec000-32cc-4ce1-a9df-22135dfe3602

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import PublicApp from "./PublicApp";
+import "./i18n/config"; // Initialize i18n
 
 // Check if we're on a public route
 const isPublicRoute = window.location.pathname.startsWith('/prospects');
